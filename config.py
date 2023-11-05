6372bbdf8a5978fbe1f60ba7879414ae1eeecece
abhi971 <70852946+abhi971@users.noreply.github.com>
2023-11-05 22:25:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from os import getenv
 
 class Config(object):
-      API_HASH = getenv("API_HASH", "a1a06a18eb9153e9dbd447cfd5da2457")
-      API_ID = int(getenv("API_ID", "20389440"))
+      API_HASH = getenv("API_HASH", "8828ed9ae1e7b1538d04d8a6de85589e")
+      API_ID = int(getenv("API_ID", "1789161"))
       AS_COPY = True if getenv("AS_COPY", True) == "`{file_name}`" else True
-      BOT_TOKEN = getenv("BOT_TOKEN", "6564513574:AAGDqUaEmeu0m4DjLDetNc4nooVTWYT7Fzo")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001722984461:-1001623633000").replace("\n", " ").split(' '))
+      BOT_TOKEN = getenv("BOT_TOKEN", "6945663522:AAGn0W9Pguj_13gUbCWyNl-h_YiXgJo-w0s")
+      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1670336143:-2116075401").replace("\n", " ").split(' '))
 
 
 # Don't Remove Credit @VJ_Botz
